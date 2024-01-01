3f3271dc967bb27260b3a926ca164ba91fe8b01c
aeldalashy <aeldalashy.c@stc.com.sa>
2024-01-01 20:42:23 +0300

    first commit

---
+++ src/main/java/ist/stc/genesys/JenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package ist.stc.genesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ist/stc/genesys/JenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ist.stc.genesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
